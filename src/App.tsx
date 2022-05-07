4e590d81445165e2687e365a49fd646a7cc99845
Swen Mulderij <swenmulderij@gmail.com>
2022-05-07 22:26:40 +0200

    added hex rendering and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { extendHex, defineGrid, Grid } from 'honeycomb-grid';
-
+import React, { useEffect, useState, ReactFragment } from "react";
+import "./App.css";
+import { extendHex, defineGrid, Grid, Hex } from "honeycomb-grid";
+import HexLeaf from "./components/Hex";
 
 function App() {
-  const [getGrid, setGrid] = useState<null | Grid>(null);
+  const [getGrid, setGrid] = useState<Grid | undefined>();
+  const [getPropMap, setPropMap] = useState({} as Record<number, any>);
+  const size = 54;
   useEffect(() => {
     const HexFactory = extendHex({
-      size: 40,
-      orientation: 'flat'
-    })
+      size: size,
+      origin: [2 * size * 0.5, Math.sqrt(3) * size * 0.5],
+      orientation: "flat",
+    });
     const GridFactory = defineGrid(HexFactory);
-    setGrid(GridFactory.hexagon({radius:3}))
-  })
+    setGrid(
+      GridFactory.hexagon({
+        radius: 2,
+      })
+    );
+  }, []);
+
+  useEffect(() => {
+    if (getGrid) {
+      setPropMap(
+        [...getGrid].reduce(
+          (prev, cur) => ({ ...prev, [getGrid.indexOf(cur)]: {} }),
+          {} as { number: any }
+        )
+      );
+    }
+  }, [getGrid]);
+
+  // useEffect(() => {
+  //   console.log(getPropMap);
+  // }, [getPropMap]);
+
   return (
     <div className="App">
-      <h1>Bouquet.</h1>
-      {getGrid ? <svg></svg> : <p>Generating...</p>}
-
-
+      <h1>Bouquet 💐</h1>
+      {getGrid && getPropMap ? (
+        <svg
+          width={size * 9}
+          height={size * 9}
+          viewBox={`-${size * 4.5} -${size * 4.5} ${size * 9} ${size * 9}`}
+        >
+          <g>
+            {[...getGrid].map((el) => (
+              <HexLeaf hex={el} prop={getPropMap[getGrid.indexOf(el)]} />
+            ))}
+          </g>
+        </svg>
+      ) : (
+        <p>Generating...</p>
+      )}
     </div>
   );
 }
---
+++ src/components/Hex.tsx
@@ -0,0 +1,39 @@
+import { Hex, Point } from "honeycomb-grid";
+import chroma from "chroma-js";
+import { useState } from "react";
+
+interface HexLeafProps {
+  hex: Hex<{}>;
+  prop: any;
+}
+
+const hexPath = (corners: Point[]) => {
+  const [first, ...others] = corners;
+  let pathStr = `M${first.x}, ${first.y} `;
+  others.forEach(({ x, y }, i) => {
+    pathStr += `L${x}, ${y} `;
+  });
+  return pathStr + " Z";
+};
+
+const HexLeaf = (props: HexLeafProps) => {
+  const [getColor, setColor] = useState(
+    chroma.scale(["#3b8bbc", "#e0915a"]).mode("lch").colors(6)
+  );
+  return (
+    <g>
+      <path
+        d={hexPath(props.hex.corners())}
+        // d={"M 0,0 L 10,10"}
+        transform={`translate(${props.hex.toPoint().x},${
+          props.hex.toPoint().y
+        })`}
+        stroke={"#ccc"}
+        strokeWidth={2}
+        fill={getColor[Math.floor(Math.random() * getColor.length)]}
+      />
+    </g>
+  );
+};
+
+export default HexLeaf;
